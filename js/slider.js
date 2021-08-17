f02fbd9c405d5a623e2bc7c48866c8ac81b4f029
Roman Kutyavin <roman.kut21@gmail.com>
2021-08-18 01:44:26 +0400

    first commit

---
+++ js/slider.js
@@ -0,0 +1,43 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+  showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+  showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("parag");
+  var dots = document.getElementsByClassName("noguma-pomoki-kadra");
+  if (n > slides.length) {
+  slideIndex = 1
+  }
+  if (n < 1) {
+  slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace("deystvuyus", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " deystvuyus";
+}
+var hover;   
+
+document.querySelector('.adaptivn-slayder')
+  .addEventListener('mousemove', () => hover = 1);
+
+document.querySelector('.adaptivn-slayder')
+  .addEventListener('mouseout', () => hover = 0);
+
+setInterval(() => !hover && plusSlide(), 4000);
